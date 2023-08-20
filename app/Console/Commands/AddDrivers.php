9416e4294b88599062e6c2abb2e5c2d536e0ad1e
T Michael Smith <t.michael.smith@toyota.com>
2023-08-20 15:31:23 -0500

    fixes autoload issue

--- app/Console/Commands/AddDrivers.php
+++ app/Console/Commands/AddDrivers.php
@@ -3,16 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Models\Driver;
-use App\Models\Race;
-use App\Models\Track;
-use Carbon\Carbon;
 use Illuminate\Http\Client\Response as GuzzleResponse;
 use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Response;
 
-class AddRaces extends Command
+class AddDrivers extends Command
 {
     /**
      * The name and signature of the console command.
