9f4ae61044926cb8c4ad1e4e46558a4c584730c7
T Michael Smith <t.michael.smith@toyota.com>
2023-08-20 10:18:11 -0500

    force https in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (env('APP_ENV') !=== 'local') {
+        if (env('APP_ENV') !== 'local') {
             Url::forceScheme('https');
         }
     }
